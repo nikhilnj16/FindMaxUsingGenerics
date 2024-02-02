3f7adee64f928b9b28a58693512d45421b04e209
Nikhil <nikiljana123@gmail.com>
2024-02-02 12:24:49 +0530

    added: FindMAXString

--- FindMaxUsingGenerics/src/com/bridgelabz/Generic/Testmaximum.java
+++ FindMaxUsingGenerics/src/com/bridgelabz/Generic/Testmaximum.java
@@ -1,45 +1,44 @@
 package com.bridgelabz.Generic;
 
-public class Testmaximum {
-    public static void main(String[] args) {
+public class Testmaximum <E extends Comparable<E>> {
+    E value1;
+    E value2;
+    E value3;
 
-        //testcase1
-        Integer result1 = findMaxInt(11, 7, 6);
-        System.out.println("TC 1: " + "Max number at 1st position " + result1);
-
-        //testcase2
-        Integer result2 = findMaxInt(23, 45, 32);
-        System.out.println("TC 2: " + "Max number at 2nd position " + result2);
-        
-        //testcase3
-        Integer result3 = findMaxInt(43, 14, 89);
-        System.out.println("TC 3: " + "Max number at 3rd position " + result3);
-
-        //testcase1
-        Float result4 = findMaxFloat(2.3f, 4.5f, 3.4f);
-        System.out.println("TC 1: " + "Max number at 1st position " + result4);
-
-        //testcase2
-        Float result5 = findMaxFloat(23f, 45f, 32f);
-        System.out.println("TC 2: " + "Max number at 2nd position " + result5);
-
-        //testcase3
-        Float result6 = findMaxFloat(43f, 14f, 89f);
-        System.out.println("TC 3: " + "Max number at 3rd position " + result6);
+    public Testmaximum(E value1, E value2, E value3) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
     }
 
-    private static Integer findMaxInt(Integer num1, Integer num2, Integer num3) {
-        Integer max = num1;
-        if (max.compareTo(num2) < 0){
-            max = num2;
+    public static void main(String[] args) {
+
+        Testmaximum <Integer> FMInt = new Testmaximum<>(54, 34, 87);
+        FMInt.testmax();
+        Testmaximum <Float> FMFloat = new Testmaximum<>(3.4f, 5.6f, 1.2f);
+        FMFloat.testmax();
+        Testmaximum <String> FMString = new Testmaximum<>("Apple", "Guaua", "Banana");
+        FMString.testmax();
+
+    }
+
+    public void testmax() {
+        E result = findMax();
+        System.out.println("max value: " + result);
+    }
+
+    private E findMax() {
+        E max = value1;
+        if (max.compareTo(value2) < 0){
+            max = value2;
         }
-        if (max.compareTo(num3) < 0) {
-            max = num3;
+        if (max.compareTo(value3) < 0) {
+            max = value3;
         }
         return max;
     }
 
-    private static Float findMaxFloat(Float num1, Float num2, Float num3) {
+    /*private static Float findMaxFloat(Float num1, Float num2, Float num3) {
         Float max = num1;
         if (max.compareTo(num2) < 0){
             max = num2;
@@ -48,7 +47,7 @@ public class Testmaximum {
             max = num3;
         }
         return max;
-    }
+    }*/
 
 
 
